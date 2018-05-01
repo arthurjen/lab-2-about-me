7b1b89282743cc7efd5beffa29e0636213a7431c
Arthur Jen <arthurljen@gmail.com>
2018-05-01 13:53:55 -0700

    ultimate game framework

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function ultimateGame() {
+    var name = prompt('Supernova!');
+    if(name.trim().toLowerCase() === 'phoenix') {
+        alert('Correct!');
+    }
+   
+    name = prompt('Epicenter!');
+    if(name.trim().toLowerCase() === 'earth shaker') {
+        alert('Correct!');
+    }
+
+    name = prompt('Exorcism!');
+    if(name.trim().toLowerCase() === 'death\'s prophet' || name.trim().toLowerCase() === 'deaths prophet') {
+        alert('Correct!');
+    }
+
+    name = prompt('Sunder!');
+    if(name.trim().toLowerCase() === 'terrorblade') {
+        alert('Correct!');
+    }
+
+    name = prompt('Omnislash!');
+    if(name.trim().toLowerCase() === 'juggernaut') {
+        alert('Correct!');
+    }
+
+    name = prompt('Eclipse!');
+    if(name.trim().toLowerCase() === 'luna') {
+        alert('Correct!');
+    }
+
+    name = prompt('Reverse Polarity!');
+    if(name.trim().toLowerCase() === 'magnus') {
+        alert('Correct!');
+    }
+
+    name = prompt('Mystic Flare!');
+    if(name.trim().toLowerCase() === 'skywrath' || name.trim().toLowerCase() === 'skywrath mage' || name.trim().toLowerCase() === 'dragonus') {
+        alert('Correct!');
+    }
+
+    name = prompt('Fiend\'s Grip!');
+    if(name.trim().toLowerCase() === 'bane') {
+        alert('Correct!');
+    }
+
+    name = prompt('Static Storm!');
+    if(name.trim().toLowerCase() === 'disruptor') {
+        alert('Correct!');
+    }
+}
