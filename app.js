87864db3bcdbc7d35cc9b77e07cbe6efd112a5eb
Arthur Jen <arthurljen@gmail.com>
2018-05-01 18:13:25 -0700

    lab complete!

--- app.js
+++ app.js
@@ -134,53 +134,53 @@ function itemGame() {
 
     var item = prompt('1. Does a Force Staff require a Ring of Regen?');
     if(item.trim().toLowerCase() === 'y' || item.trim().toLowerCase() === 'yes') {
-        alert('Correct!');
+        alert('This should come in handy!');
         score++;
         itemStars.textContent = '⭐️';
     }
     else {
-        alert('Memory is an imperfect vessel.');
+        alert('Whoops.');
         itemStars.textContent = '❌';
     }
 
     item = prompt('2. Does a Scythe of Vyse require a Staff of Wizardry?');
     if(item.trim().toLowerCase() === 'n' || item.trim().toLowerCase() === 'no') {
-        alert('Correct!');
+        alert('I agree.');
         score++;
         itemStars.textContent += '⭐️';
     }
     else {
-        alert('Memory is an imperfect vessel.');
+        alert('Bah, how forgetful of me.');
         itemStars.textContent += '❌';
     }
     item = prompt('3. Does a Veil of Discord require a Circlet?');
     if(item.trim().toLowerCase() === 'y' || item.trim().toLowerCase() === 'yes') {
-        alert('Correct!');
+        alert('Absolutely.');
         score++;
         itemStars.textContent += '⭐️';
     }
     else {
-        alert('Memory is an imperfect vessel.');
+        alert('You idiot. Ugh.');
         itemStars.textContent += '❌';
     }
     item = prompt('4. Does an Orchid Malevolence require a Quarterstaff?');
     if(item.trim().toLowerCase() === 'y' || item.trim().toLowerCase() === 'yes') {
-        alert('Correct!');
+        alert('Very good!');
         score++;
         itemStars.textContent += '⭐️';
     }
     else {
-        alert('Memory is an imperfect vessel.');
+        alert('That was your best?');
         itemStars.textContent += '❌';
     }
     item = prompt('5. Does a Shiva\'s Guard require a Helm of Iron Will?');
     if(item.trim().toLowerCase() === 'n' || item.trim().toLowerCase() === 'no') {
-        alert('Correct!');
+        alert('Friends! I think we can agree that this one is in the bag!');
         score++;
         itemStars.textContent += '⭐️';
     }
     else {
-        alert('Memory is an imperfect vessel.');
+        alert('Maybe next time.');
         itemStars.textContent += '❌';
     }
 
@@ -206,12 +206,12 @@ function invokerGame() {
     if(randomNumber === 0) {
         var spell = prompt('Coldsnap!');
         if(spell.toUpperCase() === 'QQQ') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Quas Frigoris!';
             invokerSpree++;
             invokerEmojis.textContent += '❄️';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Words fail me...';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -219,12 +219,12 @@ function invokerGame() {
     else if(randomNumber === 1) {
         spell = prompt('Ghost Walk!');
         if(spell.toUpperCase() === 'QQW' || spell.toUpperCase() === 'QWQ' || spell.toUpperCase() === 'WQQ') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Geist of Lethargy!';
             invokerSpree++;
             invokerEmojis.textContent += '👻';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Infelicitous.';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -232,7 +232,7 @@ function invokerGame() {
     else if(randomNumber === 2) {
         spell = prompt('Ice Wall!');
         if(spell.toUpperCase() === 'QQE' || spell.toUpperCase() === 'QEQ' || spell.toUpperCase() === 'EQQ') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'The harsh white waste beckons!';
             invokerSpree++;
             invokerEmojis.textContent += '🏔';
         }
@@ -245,12 +245,12 @@ function invokerGame() {
     else if(randomNumber === 3) {
         spell = prompt('EMP!');
         if(spell.toUpperCase() === 'WWW') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Extractive Mana Pulse!';
             invokerSpree++;
             invokerEmojis.textContent += '⚡️';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'This magic... disappoints.';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -258,12 +258,12 @@ function invokerGame() {
     else if(randomNumber === 4) {
         spell = prompt('Tornado!');
         if(spell.toUpperCase() === 'WWQ' || spell.toUpperCase() === 'WQW' || spell.toUpperCase() === 'QWW') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Wex cyclonus!';
             invokerSpree++;
             invokerEmojis.textContent += '🌪';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'This spell works nowise!';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -271,12 +271,12 @@ function invokerGame() {
     else if(randomNumber === 5) {
         spell = prompt('Alacrity!');
         if(spell.toUpperCase() === 'WWE' || spell.toUpperCase() === 'WEW' || spell.toUpperCase() === 'EWW') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Experience true swiftness!';
             invokerSpree++;
             invokerEmojis.textContent += '🏃‍';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Perturbations!';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -284,12 +284,12 @@ function invokerGame() {
     else if(randomNumber === 6) {
         spell = prompt('Sun Strike!');
         if(spell.toUpperCase() === 'EEE') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Harlek\'s Incantation of Incineration!';
             invokerSpree++;
             invokerEmojis.textContent += '☀️';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Short-sighted of you.';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -297,12 +297,12 @@ function invokerGame() {
     else if(randomNumber === 7) {
         spell = prompt('Forge Spirit!');
         if(spell.toUpperCase() === 'EEQ' || spell.toUpperCase() === 'EQE' || spell.toUpperCase() === 'QEE') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Ravagers of Armor and Will!';
             invokerSpree++;
             invokerEmojis.textContent += '🔥';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Ruinous, ill-fated spell.';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -310,12 +310,12 @@ function invokerGame() {
     else if(randomNumber === 8) {
         spell = prompt('Chaos Meteor!');
         if(spell.toUpperCase() === 'EEW' || spell.toUpperCase() === 'EWE' || spell.toUpperCase() === 'WEE') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Behold the meatball!';
             invokerSpree++;
             invokerEmojis.textContent += '☄️';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'My concentration... shattered!';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
@@ -323,16 +323,62 @@ function invokerGame() {
     else if(randomNumber === 9) {
         spell = prompt('Deafening Blast!');
         if(spell.toUpperCase() === 'QWE' || spell.toUpperCase() === 'QEW' || spell.toUpperCase() === 'WQE' || spell.toUpperCase() === 'WEQ' || spell.toUpperCase() === 'EQW' || spell.toUpperCase() === 'EWQ') {
-            invokerResponse.textContent = 'Impressive.';
+            invokerResponse.textContent = 'Sonic BOOM!';
             invokerSpree++;
             invokerEmojis.textContent += '🔊';
         }
         else {
-            invokerResponse.textContent = 'Lamentable.';
+            invokerResponse.textContent = 'Hahahahahahahahahahahaha.';
             invokerSpree = 0;
             invokerEmojis.textContent = '';
         }
     }
     //Display invoking spree
     invokerScore.textContent = 'You\'ve invoked ' + invokerSpree + ' spells in a row!';
+}
+
+//decalre the movespeed target outside of function
+var myMovespeed = +290;
+function movespeedGame() {
+    //initializing where the end response
+    var movespeedScore = document.getElementById('movespeed-game-score');
+
+    //create a loop of i+1 guesses 
+    for(var i = 4; i >= 0; i--) {
+        var movespeedGuess = +prompt('What is my movespeed?');
+        //check to see if it's the correct answer
+        if(movespeedGuess === myMovespeed) {
+            alert('The journey was worth it!');
+            movespeedScore.textContent = 'You guessed my movespeed! Well done.';
+            break;
+        }
+        //next check if the user has any more guesses
+        else if(i === 0) {
+            alert('You lost!');
+            movespeedScore.textContent = 'You didn\'t guess my movespeed.';
+            break;
+        }
+        //otherwise, give a hint and notify how many guesses remain
+        else if(movespeedGuess > 522) {
+            alert('You are much too kind. But, now you have ' + i + ' more guesses.');
+        }
+        else if(movespeedGuess > 340) {
+            alert('Not as fast as nature\'s grace, Enchantress. You have ' + i + ' more guesses.');
+        }
+        else if(movespeedGuess > 310) {
+            alert('I\'m only a bit slower than Anti-mage. You have ' + i + ' more guesses.');
+        }
+        else if(movespeedGuess > 290) {
+            alert('So close! You have ' + i + ' more guesses.')
+        }
+        else if(movespeedGuess < 290) {
+            alert('I\'m faster than that. Try ' + i + ' more times.');
+        }
+        else if(movespeedGuess < 0){
+            alert('What did you hope to gain from that? You have ' + i + ' more chances.');
+        }
+        else {
+            alert('Nope, guess again! You have ' + i + ' more tries.');
+        }
+    }
 }
